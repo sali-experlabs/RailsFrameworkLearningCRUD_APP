a6b3fd00ef1e4a85a16bfc0ea6ac03cfc6b64fa0
sali-experlabs <sali@experlabs.com>
2021-09-09 17:12:41 +0500

    implemented before_action for Article finding in Articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
     http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
 
     def index
         @articles = Article.all
     end
     
     def show
-        @article = Article.find(params[:id])
     end
     
     def new
@@ -14,7 +14,6 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
@@ -27,7 +26,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
 
         if(@article.update(article_params))
             redirect_to @article
@@ -37,13 +35,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
 
         redirect_to articles_path
     end
     
     private
+
+        def find_article
+            @article = Article.find(params[:id])
+        end
+
         def article_params
             params.require(:article).permit(:title, :text)
         end
