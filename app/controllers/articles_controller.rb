82a995ce3a40d4fc2f5cb245778094ba5e44f75e
sali-experlabs <sali@experlabs.com>
2021-09-09 13:01:34 +0500

    destroy functionality implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,11 +35,16 @@ class ArticlesController < ApplicationController
         end
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+    
     private
         def article_params
             params.require(:article).permit(:title, :text)
         end
-
-    # destroy action placeholder comment
 
 end
