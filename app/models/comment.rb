0ff2970addb23d0e44becbcc59c330c994c27b98
sali-experlabs <sali@experlabs.com>
2021-09-16 18:20:29 +0500

    added custom validation class to Comment Model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,19 @@
+class MyValidator < ActiveModel::Validator
+
+    def validate(record)
+        unless record.commenter.starts_with? 'X'
+            record.errors.add :commenter, "Commentor name must start with 'X'"
+        end
+    end
+
+end
+
 class Comment < ApplicationRecord
     belongs_to :article
     validates :commenter, confirmation: true
-    validates :commenter_confirmation, presence: true
+    # validates :commenter, format: { with: /\A[a-zA-Z]+\z/,
+    # message: ": only allows letters" }
+    include ActiveModel::Validations
+    validates_with MyValidator
+    # validates :commenter_confirmation, presence: true
 end
